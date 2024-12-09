f761eb93fc975dadc5b65281dd55223b4a13d401
Babysisterah <luya0013@e.ntu.edu.sg>
2024-12-09 12:43:34 +0000

    1

--- app.py
+++ app.py
@@ -2,6 +2,13 @@
 from flask import Flask
 from flask import render_template,request
 import textblob
+import google.generativeai as genai
+
+api = "AIzaSyCwT4FKWOcmcvwnYjCVBU2CQCGqc9n0Q3c"
+#api = ??
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 
 app = Flask("__name__")
 
@@ -22,7 +29,17 @@ def SA():
 def SA_result():
     q = request.form.get("q")
     r = textblob.TextBlob(q).sentiment
-    return(render_template("SA_result.html",r))
+    return(render_template("SA_result.html",r=r))
+
+@app.route("/genAI",methods=["GET","POST"])
+def genAI():
+    return(render_template("genAI.html"))
+
+@app.route("/genAI_result",methods=["GET","POST"])
+def genAI_result():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("genAI_result.html",r=r.candidates[0].content.parts[0].text))
 
 if __name__ == "__main__":
     app.run()
